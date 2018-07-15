36d548a662a9d134c604fa52315e6f675187d635
automationtesthub <automationtesthub@gmail.com>
2018-07-15 18:16:23 +0530

    afasf

--- POMFramework/src/com/vTiger/Common/commonFunctions.java
+++ POMFramework/src/com/vTiger/Common/commonFunctions.java
@@ -35,7 +35,7 @@ public class commonFunctions {
 		DateFormat format=new SimpleDateFormat("yyyyMMddhhmmss");
 		Date date=new Date();
 		String datestr=format.format(date);
-		String extentReport=System.getProperty("user.dir")+"\\test-output\\extent.html";
+		String extentReport=System.getProperty("user.dir")+"\\src\\com\\vTiger\\Reports\\extent.html";
 		reportpath=extentReport;
 		System.out.println(extentReport);
 		ExtentReports report=new ExtentReports(extentReport);
